579d272640b945f9c038cf356da7989c8547cf52
Carolina Loaiza <karolina801chan@gmail.com>
2020-05-02 20:35:44 -0600

    Added test remove connect

--- src/main/java/com/cenfotec/ColectivoJamApi/connect/Connect.java
+++ src/main/java/com/cenfotec/ColectivoJamApi/connect/Connect.java
@@ -2,12 +2,8 @@ package com.cenfotec.ColectivoJamApi.connect;
 
 import java.io.IOException;
 
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.FirestoreOptions;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.cloud.FirestoreClient;
 
 public class Connect {
 	private static Firestore db;
