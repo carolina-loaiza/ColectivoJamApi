88c9ffc9e6acd04a40eb0328e718083043bbe8c5
Carolina Loaiza <karolina801chan@gmail.com>
2020-05-02 20:47:30 -0600

    Added back connect

--- src/main/java/com/cenfotec/ColectivoJamApi/ColectivoJamApiApplication.java
+++ src/main/java/com/cenfotec/ColectivoJamApi/ColectivoJamApiApplication.java
@@ -5,11 +5,13 @@ import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cenfotec.ColectivoJamApi.connect.Connect;
+
 @SpringBootApplication
 public class ColectivoJamApiApplication {
 
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(ColectivoJamApiApplication.class, args);
-		///Connect.initConnect();
+		Connect.initConnect();
 	}
 }
